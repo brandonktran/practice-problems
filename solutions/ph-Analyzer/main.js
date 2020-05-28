2916dfaf07d42b09b7bd60bb5f51dabb9a7abac2
brandonktran <brandontran@berkeley.edu>
2020-05-28 10:50:48 -0700

    pHAnalyzer

---
+++ solutions/ph-Analyzer/main.js
@@ -0,0 +1,11 @@
+function pHAnalyzer(pH) {
+	if (ph < 0 || ph > 14) {
+		return 'invalid pH value';
+	} else if (ph === 7) {
+		return 'neutral';
+	} else if (ph > 0 && ph < 7) {
+		return 'acidic';
+	} else {
+		return 'alkaline';
+	}
+}
